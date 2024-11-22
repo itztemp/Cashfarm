5f35320534ad674b731409f049b79be539e7d8ea
itztemp <itztempy@outlook.com>
2024-11-21 17:16:16 -0800

    Update AutoCratePaid.lua

--- AutoCratePaid.lua
+++ AutoCratePaid.lua
@@ -1,4 +1,1 @@
-setclipboard([[script_key="script key here";
-loadstring(game:HttpGet("https://cashfarm.lol/AutoCratePaid.lua"))()]])
-
-game:GetService("Players").LocalPlayer:Kick("The new version was copied to your clipboard.")
+game:GetService("Players").LocalPlayer:Kick("Spacefarm: discord.gg/spacefarm")
