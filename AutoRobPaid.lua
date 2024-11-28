80cc57c55af7906ac304cacdeda75c6023c99d0f
itztemp <itztempy@outlook.com>
2024-11-28 10:55:57 -0800

    Update AutoRobPaid.lua

--- AutoRobPaid.lua
+++ AutoRobPaid.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/29c76c522a5a3fc11dc59a3ece9462d6.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6a855d4dc3cbbb2dd55cda7bb4e434d9.lua"))()
