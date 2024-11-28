4a0d01b56a575a5c62c1325a9f53d7c1b4bfa919
itztemp <itztempy@outlook.com>
2024-11-28 10:55:35 -0800

    Update AutoCrateFree.lua

--- AutoCrateFree.lua
+++ AutoCrateFree.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/eb2c48784cb5feecc6faa17a2e72405b.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ac1dcf6a96c44317fbd8abb64192f541.lua"))()
