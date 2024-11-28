1553d168bdea64381a8085eb391a0502b651e4f2
itztemp <itztempy@outlook.com>
2024-11-28 10:56:07 -0800

    Update AutoRobFree.lua

--- AutoRobFree.lua
+++ AutoRobFree.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2ffe99ee7ba8585799f5e959e13bf0db.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/09c0b2939706a22642bd3868f4478094.lua"))()
